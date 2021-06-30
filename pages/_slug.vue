18bec26de3d8d17f45e81eebc88ca8754c47095f
AndreKangas <andre.kangas@reaktiiv.com>
2021-06-30 12:30:24 +0300

    init

---
+++ pages/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">{{post.title}}</h1>
+
+        <!-- eslint.disable-next-line vue/no-v-html -->
+        <article class="py-" v-html="post.content"></article>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PostsState } from '~/store/posts'
+
+export default Vue.extend({
+    async asyncData({ store, params }) {
+    await store.dispatch('posts/getPost', params.slug)
+  },
+    computed: {
+        post() {
+            return (this.$store.state.posts as PostsState).post
+        },
+    },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="text-center">Headless WordPress Nuxt Demo</h1>
+      <div class="container py-4">
+        <div class="row">
+          <post-card v-for="post in posts" :key="post.slug" :post="post" />
+          <div class="d-flex w-100">
+            <div v-if="pageInfo.hasNextPage" class="ml-auto">
+              <NuxtLink :to="{ query: { after: pageInfo.endCursor } }">
+                <span>Next Page</span></NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PostsState } from '~/store/posts'
+
+const postsCount = 5
+
+export default Vue.extend({
+  async asyncData({ store, query }) {
+    await store.dispatch('posts/getPosts', {
+      after: query.after,
+      first: postsCount,
+    })
+  },
+  computed: {
+    posts() {
+      return (this.$store.state.posts as PostsState).posts
+    },
+    pageInfo() {
+      return (this.$store.state.posts as PostsState).pageInfo
+    },
+  },
+  watch: {
+    async $route() {
+      await this.$nuxt.refresh()
+      window.scrollTo(0, 0)
+    }
+  },
+})
+</script>
+
+<style></style>
